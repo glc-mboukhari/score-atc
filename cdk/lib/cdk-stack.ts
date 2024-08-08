81fab610c556a34912f886af7d0a200a60dff694
glc-mboukhari <mboukhari@lacentrale.fr>
2024-08-08 12:20:06 +0200

    [cdk] deploiement bucket

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,12 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { GlcStack, GlcStackProps } from '@groupelacentrale/libs-cdk';
+
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-
-export class CdkStack extends cdk.Stack {
+export class CdkStack extends GlcStack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // Define an S3 bucket
     new s3.Bucket(this, 'datascience-testatc-1', {
       versioned: true,
     });
